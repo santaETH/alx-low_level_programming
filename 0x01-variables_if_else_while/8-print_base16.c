d312e766ec654f1d00165022a19277169926b5bc
santaETH <sdamissie@yahoo.com>
2023-09-16 08:33:37 -0700

    9-print_comb

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,23 +6,22 @@
 
 int main(void)
 {
-	char c;
+        char c;
 
-	int d;
+        int d;
 
-	c = 'a';
-	d = '0';
-
-	while
-		(d < 10) {
-			putchar(d + '0');
-			d++;
-		}
-	while
-		(c < 'f') {
-		       putchar(c);
-		       c++;
-		}
-	putchar('\n');
-	return (0);
+        c = 'a';
+        d = '0';
+        while
+                (d < 10) {
+                        putchar(d + '0');
+                        d++;
+                }
+        while
+                (c <= 'f') {
+                       putchar(c);
+                       c++;
+                }
+        putchar('\n');
+        return (0);
 }

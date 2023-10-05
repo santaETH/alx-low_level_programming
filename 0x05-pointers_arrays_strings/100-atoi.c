ee11cd276ea976af3627a5879633a71bdadb244b
santaETH <sdamissie@yahoo.com>
2023-10-05 08:09:57 -0700

    100-atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - convert string into integer.
+ * @s: Type char pointer
+ * Return: Always 0.
+ */
+int _atoi(char *s)
+{
+	int num, a, flag;
+
+	flag = 1;
+	a = num = 0;
+
+	while (((s[a] < '0') || s[a] > '9') && s[a] != 0)
+	{
+		if (s[a] == '-')
+			flag = flag * -1;
+		a++;
+	}
+	while (((s[a] >= '0') && s[a] <= '9') && s[a] != 0)
+	{

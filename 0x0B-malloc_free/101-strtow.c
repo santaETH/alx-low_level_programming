b7e007a5e0d5fb2676d607d563b2147d68e7db2a
santaETH <sdamissie@yahoo.com>
2023-10-06 04:44:37 -0700

    101-strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,60 +1,27 @@
 #include <stdlib.h>
 #include "main.h"
 
-void mywstr(char **, char *);
-void cmyword(char **, char *, int, int, int);
-
 /**
- * strtow - a function that split a string to words.
+ * cmyword - creates a word from a string.
+ * @words: the words to insert to string.
  * @str: the string to work on it.
- * Return: a pointer of the splited words.
+ * @start: the start position.
+ * @end: the stop position.
+ * @index: where to start inserting the new word.
+ * Return: nothing.
  */
 
-char **strtow(char *str)
+void cmyword(char **words, char *str, int start, int end, int index)
 {
-	int i, wxwrflag, wlen;
-	char **words;
+	int i, j;
 
-	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
+	i = end - start;
+	words[index] = (char *)malloc(sizeof(char) * (i + 1));
+
+	for (j = 0; start < end; start++, j++)
 	{
-		return (NULL);
+		words[index][j] = str[start];
 	}
 
-
-	i = wxwrflag = wlen = 0;
-
-	while (str[i])
-
-	{
-		if (wxwrflag == 0 && str[i] != ' ')
-			wxwrflag = 1;
-
-		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
-
-		{
-			wxwrflag = 0;
-			wlen++;
-		}
-
-		i++;
-	}
-
-	wlen += wxwrflag == 1 ? 1 : 0;
-
-	if (wlen == 0)
-	{
-		return (NULL);
-	}
-
-
-	words = (char **)malloc(sizeof(char *) * (wlen + 1));
-
-	if (words == NULL)
-	{
-		return (NULL);
-	}
-
-	mywstr(words, str);
-	words[wlen] = NULL;
-	return (words);
+	words[index][j] = '\0';
 }

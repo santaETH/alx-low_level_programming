862cc77441bf62e75ba2c778ad82c3664e796d95
santaETH <sdamissie@yahoo.com>
2023-10-24 13:03:05 -0700

    9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Reverese a listint_t linked list.
+ * @head: Double pointer to the head of the list.
+ *
+ * Return: Reverse to the first revesed list node of a pointer.
+ */

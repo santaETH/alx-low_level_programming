ef7eedf56f41947266d99f6863973d5f4d8ac94c
santaETH <sdamissie@yahoo.com>
2023-10-25 01:25:09 -0700

    update

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,13 +3,13 @@
 /**
  * sum_listint - return the sum of all the data n in a listint_t list.
  * @head: Pointer to the head of a list.
+ *
  * Return: Sum of all data, or zero if the list is empty.
  */
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
-	listint-t *head = head;
-	
+
 	while (head != NULL)
 	{
 		sum += head->n;

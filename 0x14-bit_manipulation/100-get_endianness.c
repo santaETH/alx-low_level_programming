a61331559d912a08705452ffb6576ef563d87951
santaETH <sdamissie@yahoo.com>
2023-10-26 13:33:27 -0700

    update

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,6 +3,8 @@
 /**
  * get_endianness - program checks the endinness.
  *
+ * This function determines whether the system is big-endian or little-endian.
+ *
  * Return : 0 big endian, 1 is little endian.
  */
 

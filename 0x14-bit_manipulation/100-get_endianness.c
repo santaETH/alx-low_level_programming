5f7cfc56704c6a4523a80008022c76fc6ae7315c
santaETH <sdamissie@yahoo.com>
2023-10-26 14:05:12 -0700

    update

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,18 +2,13 @@
 
 /**
  * get_endianness - program checks the endinness.
- *
- * This function determines whether the system is big-endian or little-endian.
- *
  * Return : 0 big endian, 1 is little endian.
  */
 
 int get_endianness(void)
 {
-	int a;
-	char *b;
+	unsigned int i = 1;
+	char *b = (char *) &i;
 
-	a = 1;
-	b = (char *) &a;
 	return (*b);
 }

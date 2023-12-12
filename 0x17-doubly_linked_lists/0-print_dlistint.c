37da1130569b9e5290a3635f66511cdf03321c02
santaETH <sdamissie@yahoo.com>
2023-12-12 05:05:13 -0800

    doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,7 +10,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count;
-	
+
 	count = 0;
 
 	if (h == NULL)
@@ -26,5 +26,5 @@ size_t print_dlistint(const dlistint_t *h)
 		h = h->next;
 	}
 
-        return (count);
+	return (count);
 }

92c88cb9b03147f043a0a5998eeb56475e892ba4
santaETH <sdamissie@yahoo.com>
2023-12-16 07:40:16 -0800

    Hack th jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -p
+wget -p .. https://raw.githubusercontent.com/santaETH/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libhack.so
+expoert LD_PRELOAD="$PWD/../libhack.so"

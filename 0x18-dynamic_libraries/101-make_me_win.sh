e12832c93af5f3dbfaa650d9127af230dd1526f4
santaETH <sdamissie@yahoo.com>
2023-12-17 00:55:39 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p .. https://raw.githubusercontent.com/santaETH/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libhack.so
-expoert LD_PRELOAD="$PWD/../libhack.so"
+wget -P .. https://raw.githubusercontent.com/santaETH/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD="$PWD/../libhack.so"

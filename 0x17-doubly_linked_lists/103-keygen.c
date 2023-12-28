0ee20d5df249ccf0b56965df0eb9bcdc15c1f0cd
santaETH <sdamissie@yahoo.com>
2023-12-27 23:17:04 -0800

    Doubly linked list

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -14,7 +14,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	char password[7], *codex;
 	int len = strlen(argv[1]), i, tmp;
-	
+
 	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	
 	/* Generate the first character of the password */
